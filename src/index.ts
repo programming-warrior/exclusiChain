95de4a3ef44a09434969911516a08940eef20d4c
Yusuf <mdyusuf2521@gmail.com>
2025-03-01 23:02:44 +0530

    redis integration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import BrandRouter from './routes/brand/router';
-import prisma from './utils/db';
+import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import BrandRouter from "./routes/brand/router";
+import prisma from "./utils/db";
+import RedisClientSingleton from "./utils/redis";
 
 // Load environment variables
 dotenv.config();
@@ -15,23 +16,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
-app.use('/api/brand',BrandRouter)
+app.use("/api/brand", BrandRouter);
 
 // Sample route
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Server is live on port 4000' });
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Server is live on port 4000" });
 });
 
 // Error handling middleware
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Something went wrong!' });
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await RedisClientSingleton.getRedisClient();
+    app.listen(4000, () => {
+      console.log("Server is running on port 4000");
+    });
+  } catch (err) {
+    console.error("Error starting server:", err);
+  }
+};
+
+// Start application
+startServer().catch(console.error);
 
 export default app;
