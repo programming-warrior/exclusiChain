6e77b5c583d60f29bce81d19d6a80abd7686d8ba
Yusuf <mdyusuf2521@gmail.com>
2025-03-01 23:00:14 +0530

    ownership transfer route added

--- src/routes/product/router.ts
+++ src/routes/product/router.ts
@@ -5,15 +5,12 @@ import prisma from "../../utils/db";
 const ProductRouter = Router();
 
 ProductRouter.put("/transfer-ownership/:id", authMiddleware, async (req: any, res: any) => {
+    if(!req.user || !req.user.id) return res.status(401).json({ error: "Unauthorized" });
     const {
         id
     } = req.params;
-    
-    const {
-        user_id
-    } = req.body;
-
-    if(!user_id) return res.status(400).json({ error: "invalid input" });
+  
+    if(!req.user.id) return res.status(400).json({ error: "invalid input" });
     const product = await prisma.product.findUnique({
         where: {
             id  
@@ -22,14 +19,15 @@ ProductRouter.put("/transfer-ownership/:id", authMiddleware, async (req: any, re
     if(!product) return res.status(404).json({ error: "product not found" });
     const user = await prisma.user.findUnique({
         where: {
-            id: user_id
+            id: req.user.id
         }
     });
     if(!user) return res.status(404).json({ error: "user not found" });
-    const txHash = await aptosService.transfer(product.ownerId, user.id, product.id);
+    // const txHash = await aptosService.transfer(product.ownerId, user.id, product.id);
+
     return res.status(200).json({
-        message: "product bought successfully",
-        txHash
+        message: "product transfer request send",
+        txHash: "txHash"
     });
 });
 
